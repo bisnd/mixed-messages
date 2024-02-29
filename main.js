5ac78fc8458ea871be8f0b5aa7366888d42ca440
Bianca <bianca.mularoni@me.com>
2024-02-29 11:35:20 +0000

    Add arrays with message components

---
+++ main.js
@@ -0,0 +1,4 @@
+const arrayNames = ['Alex', 'Amber', 'Chloe', 'Chris', 'Ellie', 'Georgia', 'Jamie', 'Jess', 'Josh', 'Luke', 'Molly', 'Zack'];
+const arrayCareers = ['an account manager', 'an entrepreneur', 'a gas engineer', 'an influencer', 'a model', 'a nurse', 'a personal trainer', 'a professional footballer', 'a student', 'a tradie'];
+const arrayPlaces = ['Essex', 'London', 'Manchester', 'Scotland', 'Wales'];
+const arrayQualities = ['is athletic', 'is attractive', 'has blonde hair', 'is caring', 'is confident', 'has dark hair', 'is family oriented', 'is fit', 'is funny', 'is genuine', 'goes to the gym', 'has good banter', 'has good teeth', 'is honest', 'is kind hearted', 'is loyal', 'has a nice smile', 'is spontaneous', 'is tall']
